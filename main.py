d82dc6f5873653dc27343c6ee55465212005fd01
crnd99 <crnd99@gmail.com>
2025-05-05 02:22:17 -0500

    Completed work on the bookbot

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count
 from stats import get_char_count
 from stats import chars_to_sorted
@@ -11,19 +12,23 @@ def get_book_text(input):
 
 
 def main():
-    filepath = "books/frankenstein.txt"
-    print("============ BOOKBOT ============")
-    print("----------- Word Count ----------")
-    get_word_count(filepath)
-    print("--------- Character Count -------")
-    char_counts = get_char_count(filepath)
-    sorted_chars = chars_to_sorted(char_counts)
-    for char in sorted_chars:
-        character = char["char"]
-        count = char["num"]
-        print(f"{character}: {count}")
+    if len(sys.argv) == 2:
+        print("============ BOOKBOT ============")
+        print("----------- Word Count ----------")
+        get_word_count(sys.argv[1])
+        print("--------- Character Count -------")
+        char_counts = get_char_count(sys.argv[1])
+        sorted_chars = chars_to_sorted(char_counts)
+        for char in sorted_chars:
+            character = char["char"]
+            count = char["num"]
+            print(f"{character}: {count}")
 
-    print("============= END ===============")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
 
 
 main()
